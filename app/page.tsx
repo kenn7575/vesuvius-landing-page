549ebcef97342021b6b432608ae87a7a6a3a8890
kenni kollemorten <kenn7575@gmail.com>
2024-11-22 10:42:13 +0100

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,96 @@
-import Image from "next/image";
+"use client";
+import { Calendar } from "@/components/ui/calendar";
+import { useEffect, useState, useCallback } from "react";
+import { startOfMonth, endOfMonth, getDay } from "date-fns";
+import { enGB } from "date-fns/locale";
+
+const MODIFIER_STYLES = {
+  unavailable: { backgroundColor: "red" },
+  partiallyUnavailable: { backgroundColor: "orange" },
+};
+
+const STYLES = {
+  months: { fontSize: "2rem" },
+  caption: { fontSize: "1.2rem" },
+  caption_label: { fontSize: "1.2rem", padding: "1rem" },
+  table: { width: "100%", borderSpacing: "0.5rem" },
+  head_cell: { width: "4rem", height: "4rem", fontSize: "1rem" },
+  cell: { width: "4rem", height: "4rem" },
+  day: { width: "4rem", height: "4rem", fontSize: "1.2rem" },
+  nav_button_previous: { width: "4rem", height: "4rem" },
+  nav_button_next: { width: "4rem", height: "4rem" },
+};
+
+async function fetchAvailabilityData(
+  startDate: Date,
+  endDate: Date,
+  setModifiers
+) {
+  try {
+    const startDateString = startDate.toISOString().split("T")[0];
+    const endDateString = endDate.toISOString().split("T")[0];
+
+    const backendUrl = process.env.BACKEND_URL || "http://localhost:5005";
+    const response = await fetch(
+      `${backendUrl}/reservations/range?start_date=${startDateString}&end_date=${endDateString}`
+    );
+    const data = await response.json();
+    console.log("🚀 ~ data:", data);
+
+    const unavailable = data
+      .filter((item) => item.availability === "Unavailable")
+      .map((item) => new Date(item.date));
+    const partiallyUnavailable = data
+      .filter((item) => item.availability === "PartiallyAvailable")
+      .map((item) => new Date(item.date));
+
+    setModifiers({ unavailable, partiallyUnavailable });
+  } catch (error) {
+    console.error("Error fetching availability data:", error);
+  }
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [month, setMonth] = useState(new Date());
+  const [startDate, setStartDate] = useState(startOfMonth(month));
+  const [endDate, setEndDate] = useState(endOfMonth(month));
+  const [modifiers, setModifiers] = useState({
+    unavailable: [],
+    partiallyUnavailable: [],
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  useEffect(() => {
+    const firstDayOfWeek = getDay(startOfMonth(month)) || 7;
+    const daysToSubtract = firstDayOfWeek - 1;
+    const firstVisibleDate = new Date(startOfMonth(month));
+    firstVisibleDate.setDate(firstVisibleDate.getDate() - daysToSubtract);
+
+    const lastDayOfWeek = getDay(endOfMonth(month)) || 7;
+    const daysToAdd = 7 - lastDayOfWeek;
+    const lastVisibleDate = new Date(endOfMonth(month));
+    lastVisibleDate.setDate(lastVisibleDate.getDate() + daysToAdd);
+
+    setStartDate(firstVisibleDate);
+    setEndDate(lastVisibleDate);
+  }, [month]);
+
+  useEffect(() => {
+    fetchAvailabilityData(startDate, endDate, setModifiers);
+    console.log(modifiers);
+  }, [startDate]);
+
+  const handleMonthChange = useCallback((newMonth) => setMonth(newMonth), []);
+
+  return (
+    <Calendar
+      locale={enGB}
+      month={month}
+      weekStartsOn={1}
+      onMonthChange={handleMonthChange}
+      className="rounded-md border w-fit"
+      modifiersStyles={MODIFIER_STYLES}
+      modifiers={modifiers}
+      styles={STYLES}
+    />
   );
 }
